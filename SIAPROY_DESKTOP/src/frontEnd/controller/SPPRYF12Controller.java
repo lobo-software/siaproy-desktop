d8df104019cc7b90b16d7dbf3931f0aa64dc9611
Ceciliacortez <ceciliac@lobos.com.mx>
2016-04-15 10:32:00 -0500

    COMMIT CON MASTER 2016_04_15
    CONFLICTO:
    SPPRYF12Controller.java
    Actividades.java

--- SIAPROY_DESKTOP/src/frontEnd/controller/SPPRYF12Controller.java
+++ SIAPROY_DESKTOP/src/frontEnd/controller/SPPRYF12Controller.java
@@ -27,6 +27,7 @@
    12/Apr/2016 10:03 CCL (LOBO_000076): Se eliminó código inecesario dentro del  controller y se implemnto codigo el codigopara la columna tiempo.
    14/Apr/2016 09:55 CCl(LOBO_000076: Se agraga funcionalidad al boton que contine el Stopwatch  dentro de este Controller se manda llamar la misma Clase Sw.
    14/Apr/2016 09:55 CCL(LOBO_000076):Se da formato a este dcomunto.
+   14/Apr/2016 17:11 SVA (LOBO_000076): Se añade funcionalidad con clases genéricas para la edición del TableView / Se restructura el archivo
  
  */
 package frontEnd.controller;
@@ -47,18 +48,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
-import javafx.util.Callback;
 import frontEnd.model.Actividades;
-import frontEnd.model.ComboBoxEditingCell;
-import frontEnd.model.EditingCell;
-import frontEnd.model.Typ;
 import frontEnd.model.stopWatch.Stopwatch;
+import frontEnd.util.SialComboCellFactory;
+import frontEnd.util.SialStringCellFactory;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -80,9 +78,9 @@ public class SPPRYF12Controller implements Initializable {
     @FXML
     private TableColumn<Actividades, Stopwatch> colTimer;
     @FXML
-    private TableColumn<Actividades, Typ> colProyecto;
+    private TableColumn<Actividades, String> colProyecto;
     @FXML
-    private TableColumn<Actividades, Typ> colActividades;
+    private TableColumn<Actividades, String> colActividades;
     @FXML
     private TableColumn<Actividades, String> colTiempo;
     @FXML
@@ -104,9 +102,10 @@ public class SPPRYF12Controller implements Initializable {
     @FXML
     private HBox hboxBotonPlayAgregar;
     // VARIABLES 
-    private ObservableList<Typ> typProyectos;
-    private ObservableList<Typ> typActividades;
+    private ObservableList<String> datosComboProyecto;
+    private ObservableList<String> datosComboActividades;
     private final Font fuenteReloj = Font.loadFont(Stopwatch.class.getResource("digital-7_mono.ttf").toExternalForm(), 24);
+<<<<<<< HEAD
     public boolean running;
 
     @Override
@@ -116,28 +115,42 @@ public class SPPRYF12Controller implements Initializable {
         // Se  cargan los componentes en el BodrerPane 2
         //Se empieza con la codificacion en  boton del StopWatch.
         //Configuración del TextField Total
+=======
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+>>>>>>> refs/remotes/origin/master
         tfTotal.setText("00:00:00");
         tfTotal.setFont(fuenteReloj);
-        //Configuración del Incio
         tftiempoInicio.setText("00:00:00");
         tftiempoInicio.setFont(fuenteReloj);
+<<<<<<< HEAD
         //Configuración del Final
         tfFinActividad.setText("00:00:00");
         tfFinActividad.setFont(fuenteReloj);
         ///Se a agrega esta linea de código para poner el icono de playStop en el boton.     
         AwesomeDude.setIcon(btnStarStop, AwesomeIcon.PLAY);
         //Seañade la fucnionalidad para mandar llamar la Clase Stopwatch, y  se añaden los parametros en el metdo contructor.
+=======
+        tfFinActividad.setText("00:00:00");
+        tfFinActividad.setFont(fuenteReloj);
+        AwesomeDude.setIcon(btnStarStop, AwesomeIcon.PLAY);
+>>>>>>> refs/remotes/origin/master
         Stopwatch stopWatch = new Stopwatch(tfTotal, tftiempoInicio, tfFinActividad);
         stopWatch.startStop.setMinWidth(61);
         stopWatch.startStop.setMinHeight(65);
         hboxBotonPlayAgregar.getChildren().removeAll(btnStarStop, btnAgragarAtividad);
         hboxBotonPlayAgregar.getChildren().addAll(stopWatch, btnAgragarAtividad);
+<<<<<<< HEAD
         //Se carga inicializa el componente del DatePicker
+=======
+>>>>>>> refs/remotes/origin/master
         dtfFechaActual.setValue(LocalDate.now());
         DatePickerSkin datePickerSkin = new DatePickerSkin((dtfFechaActual));
         Node popupContent = datePickerSkin.getPopupContent();
         layoutSecundario.setRight(popupContent);
         muestraFecha();
+<<<<<<< HEAD
         //Se crea el comboBox para la columna de Proyecto en  el TableView.     
         typProyectos
                 = FXCollections.observableArrayList(
@@ -170,15 +183,48 @@ public class SPPRYF12Controller implements Initializable {
         colTiempo.setCellValueFactory(new PropertyValueFactory<>("tiempo")); // Cambiar biar los  valores 
         colTiempo.setCellFactory(cellFactory);
         colTiempo.setOnEditCommit(
+=======
+        //Store proyectos
+        datosComboProyecto
+                = FXCollections.observableArrayList(
+                        "DMS_2014",
+                        "DMS_2015",
+                        "DMS_2016");
+        //Store actividades.
+        datosComboActividades
+                = FXCollections.observableArrayList(
+                        "INVESTIGACION",
+                        "REUNIÓN DE ESTÁNDARES",
+                        "APOYO AL EQUIPO DE TRABAJO");
+
+        SialStringCellFactory<Actividades, String> textFieldCell = new SialStringCellFactory<>();
+        SialComboCellFactory<Actividades, String> comboBoxCell = new SialComboCellFactory<>();
+        colProyecto.setCellValueFactory(new PropertyValueFactory<>("proyecto"));
+        colProyecto.setCellFactory(comboBoxCell.creaComboBox(datosComboProyecto));
+        colProyecto.setOnEditCommit(
+>>>>>>> refs/remotes/origin/master
                 (TableColumn.CellEditEvent<Actividades, String> t) -> {
+                    String[] clave = t.getNewValue().split("-");
+
                     ((Actividades) t.getTableView().getItems()
                     .get(t.getTablePosition().getRow()))
-                    .setTiempo(t.getNewValue());
-                });
+                    .setProyecto(clave[0]);
 
-        ////Iniciar  la descripción columna Descripción.
-        colDescripcion.setCellValueFactory(new PropertyValueFactory<>("descripcion")); // Cambiar biar los  valores 
-        colDescripcion.setCellFactory(cellFactory);
+                    System.out.println("Carga combo secundario (parametro):" + t.getNewValue());
+                });
+        colActividades.setCellValueFactory(new PropertyValueFactory<>("actividad"));
+        colActividades.setCellFactory(comboBoxCell.creaComboBoxWithCascade(datosComboProyecto, datosComboActividades));
+        colActividades.setOnEditStart((TableColumn.CellEditEvent<Actividades, String> t) -> {
+            String proyecto = ((Actividades) t.getTableView().getItems()
+                    .get(t.getTablePosition().getRow()))
+                    .getProyecto();
+
+            System.out.println("Carga combo secundario (parametro):" + proyecto);
+        });
+
+        colTiempo.setCellValueFactory(new PropertyValueFactory<>("tiempo"));
+        colDescripcion.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+        colDescripcion.setCellFactory(textFieldCell.creaTextField());
         colDescripcion.setOnEditCommit(
                 (TableColumn.CellEditEvent<Actividades, String> t) -> {
                     ((Actividades) t.getTableView().getItems()
@@ -186,20 +232,29 @@ public class SPPRYF12Controller implements Initializable {
                     .setDescripcion(t.getNewValue());
                 });
 
+<<<<<<< HEAD
         ////Iniciar  con la columna  Avance
+=======
+>>>>>>> refs/remotes/origin/master
         colAvance.setCellValueFactory(new PropertyValueFactory<>("avance"));
-        colAvance.setCellFactory(cellFactory);
+        colAvance.setCellFactory(textFieldCell.creaTextField());
         colAvance.setOnEditCommit(
                 (TableColumn.CellEditEvent<Actividades, String> t) -> {
                     ((Actividades) t.getTableView().getItems()
                     .get(t.getTablePosition().getRow()))
-                    .setPorcentajeAvance(t.getNewValue());
+                    .setAvance(t.getNewValue());
                 });
+<<<<<<< HEAD
         colTimer.setCellValueFactory(new PropertyValueFactory<>("stopWatch"));
         //Carga de registros de actividades
         grdActividades.setItems(cargaRegistrosExistentes());
     }
 //Creación  de los diferentes  métodos. //Metodo donde para mostrar el datePicker de manera Statica al momento de ejecutar la API.
+=======
+        //Carga de registros de actividades
+        grdActividades.setItems(cargaRegistrosExistentes());
+    }
+>>>>>>> refs/remotes/origin/master
 
     public void muestraFecha() {
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
@@ -215,6 +270,7 @@ public class SPPRYF12Controller implements Initializable {
 
     public ObservableList<Actividades> cargaRegistrosExistentes() {
         ObservableList<Actividades> registros = FXCollections.observableArrayList();
+<<<<<<< HEAD
         registros.add(new Actividades(new Stopwatch(tfTotal, tftiempoInicio, tfFinActividad), typProyectos.get(0), typActividades.get(0), "12:10:34", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
         registros.add(new Actividades(new Stopwatch(tfTotal, tftiempoInicio, tfFinActividad), typProyectos.get(1), typActividades.get(1), "13:10:45", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
         registros.add(new Actividades(new Stopwatch(tfTotal, tftiempoInicio, tfFinActividad), typProyectos.get(2), typActividades.get(2), "09:10:00", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
@@ -246,4 +302,13 @@ public class SPPRYF12Controller implements Initializable {
             System.out.println("cerrar pantalla");
         }
     }
+=======
+        registros.add(new Actividades("DMS_2014", "INVESTIGACION", "00:10:00", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
+        registros.add(new Actividades("DMS_2014", "REUNIÓN DE ESTÁNDARES", "00:10:00", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
+        registros.add(new Actividades("DMS_2015", "APOYO AL EQUIPO DE TRABAJO", "00:10:00", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
+        registros.add(new Actividades("DMS_2016", "DOCUMENTACIÓN", "00:10:00", "CAPTURA ACTIVIDADES 06/ABR/2016", "100"));
+
+        return registros;
+    }
+>>>>>>> refs/remotes/origin/master
 }
